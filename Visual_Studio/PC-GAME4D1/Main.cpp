cb12d71ff60c8a0245e72341bfc9e184b9300f38
Giocator0 <Giocator0@users.noreply.github.com>
2014-04-26 12:21:59 +0200

    Ora il programma gestisce le texture e gli sprites
    Funzionano anche gli atlas di textures, textures di sprites e texture
    normali

--- Visual_Studio/PC-GAME4D1/Main.cpp
+++ Visual_Studio/PC-GAME4D1/Main.cpp
@@ -4,30 +4,49 @@
 #include <string>
 #include <fstream>
 
+
 using namespace std;
 bool inizializza();
 bool caricaTexturePNG(char*);
+SDL_Texture* caricaMedia(char*);
 void chiudi();
-SDL_Texture* CaricaTexture(char*);
+SDL_Texture* caricaTexture(char*);
 
 int LARGHEZZA_SCHERMO = 800;
-int ALTEZZA_SCHERMO = 800;
+int ALTEZZA_SCHERMO = 600;
 SDL_Window* gFinestra = NULL; 
 SDL_Renderer* gRenderizzatore = NULL;
 SDL_Texture* gTexture = NULL;
-
 
 int main( int argc, char* args[] )
 {
 	bool esci = false;
 	SDL_Event e;
 	inizializza();
-	int r,c,nr,nc,x=0,y=0;
-	nc = 800;
-	nr = 800;
+	int nr,nc,x=0,y=0,sprite;
+	nc = 50;
+	nr = 50;
+	
+	SDL_Texture* textLol = NULL;
+	SDL_Texture* textAsd = NULL;
+	SDL_Texture* textQwe = NULL;
 
-	r = ALTEZZA_SCHERMO/nr;
-	c = LARGHEZZA_SCHERMO/nc;
+	textLol = caricaMedia("../Textures/brick.png");
+	textAsd = caricaMedia("../Textures/lava_flow.png");
+	textQwe = caricaMedia("../Textures/terrain.jpg");
+
+	SDL_Rect txt1 = {0,0,16,16};
+	SDL_Rect txt2 [512];
+
+	for(sprite = 0;sprite <512; sprite++){
+		txt2[sprite].x = 0;
+		txt2[sprite].y = sprite;
+		txt2[sprite].h = 16;
+		txt2[sprite].w = 16;
+	}
+
+		int frame = 0;
+
 	while(!esci) {
 		while(SDL_PollEvent( &e ) != 0 )
 			if( e.type == SDL_QUIT ) 
@@ -35,31 +54,43 @@ int main( int argc, char* args[] )
 
 		SDL_SetRenderDrawColor( gRenderizzatore, 0xFF, 0xFF, 0xFF, 0xFF );
 		SDL_RenderClear( gRenderizzatore );
-		int i= 0,j;
-		bool lol = true;
+		int i= 0;
+		bool test = true;
 		y = 0;
+		/*
 		for(;i < nr ; i++){
 			x = 0;
 			j = 0;
 			for(; j < nc; j++){	
-				lol = !lol;
-				if(lol){
-					SDL_Rect fillRect = {x,y,c,r}; 
-					SDL_SetRenderDrawColor( gRenderizzatore, ((rand()%(255-0))+255), ((rand()%(255-0))+255),((rand()%(255-0))+255), 0xFF );
-					SDL_RenderFillRect( gRenderizzatore, &fillRect );
+					if (test){
+						SDL_Rect fillRect = {x,y,16,16}; 
+						SDL_RenderCopy(gRenderizzatore,textQwe,&txt1,&fillRect);
+						SDL_RenderDrawRect(gRenderizzatore,NULL);
+						
+					}else{*/
+						SDL_Rect fillRect = {x,y,128,128};
+						SDL_Rect* currentClip = &txt2[frame /32];
+						SDL_RenderCopy(gRenderizzatore,textAsd,currentClip,&fillRect);
+						SDL_RenderDrawRect(gRenderizzatore,&fillRect);
+						if(frame == 511)
+							frame = 0;
+						else
+							frame++;/*
+					}
+					test = !test;
+					x = x+16;
 				}
-				else
-				{
-					SDL_Rect fillRect = {x,y,c,r}; 
-					SDL_SetRenderDrawColor( gRenderizzatore, 0xFF, 0xFF, 0xFF, 0xFF );
-					SDL_RenderFillRect( gRenderizzatore, &fillRect );
-				}
-				x = x+ c;
-			}
-			y = y+c;
-		}
+				
+			y = y+16;
+		}*/
 		SDL_RenderPresent( gRenderizzatore );
 	}
+	SDL_DestroyTexture(textLol); 	
+	SDL_DestroyTexture(textAsd);
+	SDL_DestroyTexture(textQwe);
+	textLol = NULL;
+	textAsd = NULL;
+	textQwe = NULL;
 	chiudi();
 	return 0;
 }
@@ -77,7 +108,7 @@ bool inizializza() {
 			flag = false; 
 		}
 		else { 
-			gRenderizzatore = SDL_CreateRenderer(gFinestra,-1,SDL_RENDERER_ACCELERATED);
+			gRenderizzatore = SDL_CreateRenderer(gFinestra,-1,SDL_RENDERER_ACCELERATED| SDL_RENDERER_PRESENTVSYNC);
 			SDL_SetRenderDrawColor(gRenderizzatore,0xFF,0xFF,0xFF,SDL_ALPHA_OPAQUE);
 			int imgF = IMG_INIT_PNG;
 			if( !( IMG_Init( imgF ) & imgF ) ) {
@@ -88,17 +119,31 @@ bool inizializza() {
 	} 
 	return flag; 
 }
+SDL_Texture* caricaMedia(char* percorso) {
+	SDL_Texture* text;
+	text = caricaTexture(percorso); 
+	if( text == NULL ) { 
+		printf( "Failed to load texture image!\n" ); 
+	} 
+	return text;
+}
+
+
 
 void chiudi() { 
+	 SDL_DestroyTexture( gTexture ); 
+	 gTexture = NULL;
+
 	SDL_DestroyRenderer( gRenderizzatore );
 	SDL_DestroyWindow( gFinestra );
 	gFinestra = NULL;
 	gRenderizzatore = NULL;
+
 	IMG_Quit();
 	SDL_Quit();
 }
 
-SDL_Texture* CaricaTexture(char* percorso){
+SDL_Texture* caricaTexture(char* percorso){
 	
 	SDL_Texture* tmp = NULL;
 	SDL_Surface* sCarica = IMG_Load(percorso);
